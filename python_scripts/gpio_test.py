5beb80621573e7f137b64512241d57824cb44733
Anatoly Mironov <mirontoli@gmail.com>
2015-09-10 07:42:52 +0200

    add comments to the code

--- python_scripts/gpio_test.py
+++ python_scripts/gpio_test.py
@@ -1,3 +1,9 @@
+#2015-09-10
+#pin 7 (+) goes to a led light then resistor
+#then to the pin 6 (gnd)
+#inspired by Explaining Computers: 
+#Raspberry Pi Robotics #1: GPIO Control
+#https://www.youtube.com/watch?v=41IO4Qe5Jzw
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
