8118405a7038c4640bd09a316fb187fe78fbe888
Anatoly Mironov <mirontoli@gmail.com>
2015-11-02 22:13:48 +0000

    fix indentation and fix some issues

--- python_scripts/google_sense.py
+++ python_scripts/google_sense.py
@@ -17,6 +17,7 @@
 import time
 from sense_hat import SenseHat
 from datetime import datetime
+import json
 import gspread
 from oauth2client.client import SignedJwtAssertionCredentials
 
@@ -29,22 +30,22 @@ scope = ['https://spreadsheets.google.com/feeds']
 credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
 gc = gspread.authorize(credentials)
 sh = gc.open("climateData")
-worksheet = sh.get_worksheet(0).
+worksheet = sh.get_worksheet(0)
 a1 = worksheet.acell('A1').value
 if a1 != 'Timestamp':
-	worksheet.resize(1)
-	worksheet.append_row(('Timestamp', 'Temperature', 'Humidity', 'Pressure'))
+  worksheet.resize(1)
+  worksheet.append_row(('Timestamp', 'Temperature', 'Humidity', 'Pressure'))
 while True :
-	sense.show_letter('S', text_colour=[0, 114, 198])
-	date = datetime.now()
-	iso_date = date.isoformat()    
-	raw_temp = sense.temp
-    #calculate temperature https://www.raspberrypi.org/forums/viewtopic.php?t=111457&p=769672
-    calctemp = 0.0071 * raw_temp * raw_temp + 0.86 * raw_temp - 10.0
-    temp = "{0:.2f}".format(calctemp)
-    humidity = "{0:.2f}".format(sense.humidity)
-    pressure = "{0:.2f}".format(sense.pressure)
-	worksheet.append_row((datetime.now(), temp, humidity, pressure))
-	time.sleep(2) #sleep 2 minutes, otherwise it goes to fast for the poc :)
-	sense.clear()
-	time.sleep(58) #clear sense
+  sense.show_letter('S', text_colour=[0, 114, 198])
+  date = datetime.now()
+  iso_date = date.isoformat()    
+  raw_temp = sense.temp
+  #calculate temperature https://www.raspberrypi.org/forums/viewtopic.php?t=111457&p=769672
+  calctemp = 0.0071 * raw_temp * raw_temp + 0.86 * raw_temp - 10.0
+  temp = "{0:.2f}".format(calctemp)
+  humidity = "{0:.2f}".format(sense.humidity)
+  pressure = "{0:.2f}".format(sense.pressure)
+  worksheet.append_row((datetime.now(), temp, humidity, pressure))
+  time.sleep(2) #sleep 2 minutes, otherwise it goes to fast for the poc :)
+  sense.clear()
+  time.sleep(58) #clear sense
