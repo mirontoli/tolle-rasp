ecb6d8947619e6aa9a009abcfc81b1d81f414f4b
Anatoly Mironov <mirontoli@gmail.com>
2021-03-21 22:46:41 +0100

    Update rfid_read.py

--- python_scripts/rfid_read.py
+++ python_scripts/rfid_read.py
@@ -1,3 +1,9 @@
+"""
+This is the code from a tutorial: https://pimylifeup.com/raspberry-pi-rfid-rc522/
+you need to enable SPI on you Raspberry Pi
+you need to install spidev and mfrc522 with pip3
+Tried on my RPi3
+"""
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
