a00117d5e70377ff3004b91649e8962a2e5aa89a
Anatoly Mironov <mirontoli@gmail.com>
2023-07-31 13:29:05 +0200

    my first discord bot

---
+++ python_scripts/tollediscordbot001/README.md
@@ -0,0 +1,24 @@
+# My first Discord Bot
+
+It just responds to messages and lists members. It is just my first test.
+
+## Installation and requirements
+
+Python 3.9 and higher is requireed, get the code, create a virtual environment and install the pip packages
+
+```bash
+python -m venv tdb001-venv
+pip install -r requirements.txt
+```
+
+In Visual Studio Code the virtual environment will be loaded automatically, so just press F5.
+
+If in terminal, activate the virtual environment
+
+```powershell
+.\tdb001-venv\Scripts\activate.ps1
+```
+
+```bash
+source tdb001-venv\bin\activate
+```
---
+++ python_scripts/tollediscordbot001/bot.py
@@ -0,0 +1,61 @@
+import os
+import random
+import discord
+from dotenv import load_dotenv
+import urllib.request
+
+with open('wisdom.txt', 'r', encoding='UTF-8') as txt_file:
+    words_of_wisdom = txt_file.readlines()
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+print(f'token is {TOKEN}')
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+
+headers = {
+    'Accept': 'text/plain',
+    'User-Agent': 'tolle-rasp (https://github.com/mirontoli/tolle-rasp)' #if not provided you'll get 403 Forbidden
+    }
+
+url = 'https://icanhazdadjoke.com/'
+request = urllib.request.Request(url, headers=headers)
+def fetch_random_joke():
+    resp = urllib.request.urlopen(request)
+    contents = resp.read().decode('UTF-8') # if not decoded the text is prepended with b'
+    return contents
+
+@client.event
+async def on_ready():
+    guild = client.guilds[0] # assumption, TODO find right guild
+    members = '\n - '.join([member.name for member in guild.members])
+    print(
+        f'{client.user} has connected to Discord!\n'
+        f'guild.name: {guild.name}\n'
+        f'Guild members:\n - {members}'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        print(f'message.author: {message.author}')
+        return
+    response = "tmp"
+    #print(f'message: {message}')
+    print(f'message.content: {message.content}')
+    if message.content.lower() == "hej":
+        response = 'Hej på dig, du 👋👋!\nDu kan testa att skriva "visdom" eller "joke"'
+    if message.content.lower() == 'visdom':
+        response = random.choice(words_of_wisdom)
+    if message.content.lower() == 'joke':
+        #response = "You'll soon hear a joke"
+        response = fetch_random_joke()
+
+    await message.channel.send(response)
+
+        
+
+client.run(TOKEN)
