efea2d24bf8b1c3d5f2fdd608590e9f16840a079
Anatoly Mironov <mirontoli@gmail.com>
2023-08-01 13:31:14 +0200

    introduce food suggestions in discord bot

--- python_scripts/tollediscordbot001/bot.py
+++ python_scripts/tollediscordbot001/bot.py
@@ -7,6 +7,9 @@ import urllib.request
 with open('wisdom.txt', 'r', encoding='UTF-8') as txt_file:
     words_of_wisdom = txt_file.readlines()
 
+with open('food.txt', 'r', encoding='UTF-8') as txt_file2:
+    dishes = txt_file2.readlines()
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 print(f'token is {TOKEN}')
@@ -47,12 +50,15 @@ async def on_message(message):
     #print(f'message: {message}')
     print(f'message.content: {message.content}')
     if message.content.lower() == "hej":
-        response = 'Hej på dig, du 👋👋!\nDu kan testa att skriva "visdom" eller "joke"'
+        response = 'Hej på dig, du 👋👋!\nDu kan testa att skriva "visdom", "mat" eller "joke"'
     if message.content.lower() == 'visdom':
         response = random.choice(words_of_wisdom)
     if message.content.lower() == 'joke':
         #response = "You'll soon hear a joke"
         response = fetch_random_joke()
+    if message.content.lower() == 'mat':
+        dish = random.choice(dishes)
+        response = f'{dish}\nGlöm inte att äta grönsaker'
 
     await message.channel.send(response)
 
